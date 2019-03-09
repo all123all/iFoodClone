a5bf5eb1375591aa2d1bfd91d858c0427d4f57a9
all123all <allyson2308h@gmail.com>
2019-03-09 02:20:49 -0300

    Tela de Login

--- App.js
+++ App.js
@@ -1,12 +1,6 @@
-//  #12090C
-//  #1C0E12
-//  #DDD3BA
-//  #2B171C
-//  #070405
-//  #8C8272
-
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, TextInput, Image, } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity,} from 'react-native';
+import { Image } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
@@ -18,23 +12,32 @@ export default class App extends React.Component {
             </Text>
         </View>
 
-        <ScrollView>
-          
+        <ScrollView>          
           <View style={styles.inputPanel}>
-            <Image
-              source={require('./assets/img/os.gif')}
-              style={styles.inputPanelLogo}
+            <View style={styles.inputPanelLogo}>
+              <Image 
+                source={require('./assets/img/os1.gif')}
+                containerStyle={styles.inputPanelLogoPic}
+              />
+            </View>
+            <TextInput
+              placeholderTextColor="#070304"
+              style={styles.inputData}
+              placeholder="u s e r"
+              placeholderTextColor="#8C8272"
             />
             <TextInput
               placeholderTextColor="#070304"
               style={styles.inputData}
-              placeholder="Username:"
+              placeholder="p a s s c o d e"
+              placeholderTextColor="#8C8272"
             />
-            <TextInput
-              placeholderTextColor="#070304"
-              style={styles.inputData}
-              placeholder="PassWord:"
-            />
+            <TouchableOpacity style={styles.btnLogin}>
+              <Text style={{color: '#BCB193', padding: '2%'}}>L o g i n</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.btnLogin}>
+              <Text style={{color: '#BCB193', padding: '2%'}}>S i g n u p</Text>
+            </TouchableOpacity>
           </View>
         </ScrollView>
       </View>
@@ -59,16 +62,37 @@ const styles = StyleSheet.create({
   topPanelText:{
     color: '#BCB193',
   },
-  inputPanelLogo:{
-    resizeMode: 'contain',
-    width: '40%'
-  },
   inputPanel:{
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
     justifyContent: 'center',
     alignItems: 'center',
   },
   inputData:{    
     backgroundColor: '#DDD3BA',
     width: '60%',
+    marginTop: '2%',
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#BCB193',
+    padding: '2%',
+  },
+  btnLogin:{
+    width: '60%',
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#BCB193',
+    backgroundColor: 'transparent',
+    marginTop: '2%',
+    alignItems: 'center',
   },
 });
+
+//  #12090C
+//  #1C0E12
+//  #DDD3BA
+//  #2B171C
+//  #070405
+//  #8C8272
